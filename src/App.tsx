7c9975e8ee1afabe0a09ed94ee07a281f1652be2
Yuval Allweil <themarquisdesheric@gmail.com>
2022-01-16 16:21:45 -0800

    install tailwind (yes, this is probably overkill)

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>let's play with some audio input</h1>
+    <div className="App min-h-screen flex flex-col justify-center items-center text-center">
+      <p className="text-3xl font-bold">let's play with some audio input</p>
     </div>
   );
 }
